6d545c156b4e0258eb0e21d771b677a40f09dc2f
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2021-09-03 23:13:11 +0000

    signins

---
+++ lib/Authentication screens/Screens/Signup/signup_screen.dart
@@ -0,0 +1,11 @@
+import 'package:digisafe_app/Authentication%20screens/Screens/Signup/components/body.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
